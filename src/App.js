76566777067439dd3d06b63c34e4ef19d4482aab
Adam Aguilera <adamaguilera@Mac.lan>
2024-12-12 09:31:41 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import './App.css';
+import { aboutMe, aboutMeContinued} from './text';
 
 function App() {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const scrollToSection = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="App">
+      <nav className="App-nav">
+        <ul>
+          <li><button onClick={() => scrollToSection('about')}>About Me</button></li>
+          <li><button onClick={() => scrollToSection('projects')}>Projects</button></li>
+        </ul>
+        <div className="nav-bar">
+          <div className="nav-logo">
+            <img src="/images/logo.png" alt="Logo" onClick={scrollToTop} style={{ cursor: 'pointer' }} />
+          </div>
+        </div>
+      </nav>
       <header className="App-header">
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is a basic template for a personal portfolio.</p>
+        <div className="header-text">
+          <h1>Adam Aguilera</h1>
+          <p>welcome to my personal webpage</p>
+          <div className="icon-row">
+            <a href="https://github.com/adamaguilera" target="_blank" rel="noopener noreferrer">
+              <img src="/images/github.svg" alt="github" className="icon" />
+            </a>
+            <a href="mailto:adamkaguilera@gmail.com">
+              <img src="/images/gmail.svg" alt="email" className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/adamkaguilera" target="_blank" rel="noopener noreferrer">
+              <img src="/images/linkedin.png" alt="linkedin" className="icon" />
+            </a>
+          </div>
+        </div>
+        <img src="/images/adam.JPG" alt="Adam" className="header-image" />
       </header>
-      <section>
+      <section id="about" className="about-me">
         <h2>About Me</h2>
-        <p>Write something about yourself here.</p>
+        <p>{aboutMe}</p>
       </section>
-      <section>
+      <section id="projects">
         <h2>Projects</h2>
-        <p>Showcase your projects here.</p>
-      </section>
-      <section>
-        <h2>Contact</h2>
-        <p>Provide your contact information here.</p>
+        <p>Section is still WIP!</p>
       </section>
     </div>
   );
---
+++ src/text.js
@@ -0,0 +1,3 @@
+export const aboutMe = "I'm a software engineer based out of New York City. " +
+"I work at Lyft as a backend developer for the Lyft Business product. " +
+"With my three years of tenure I've worked on fraud prevention, payment architecture and building platform services. ";
